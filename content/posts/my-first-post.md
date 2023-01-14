7e005be201db11949bde9e76cb43176a881150bb
Jonathan Carnall <jonathancarnall@hotmail.com>
2023-01-14 20:03:30 +0000

    added a post

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,5 @@
+---
+title: "My First Post"
+date: 2023-01-14T20:02:31Z
+draft: false
+---
--- quickstart.ps1
+++ quickstart.ps1
@@ -10,3 +10,7 @@ git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke themes/an
 echo "theme = 'ananke'" >> config.toml
 
 hugo server
+
+hugo new posts/my-first-post.md
+
+hugo server
